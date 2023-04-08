dafa04cbd5102e3bed1aa1f581c2a6acb9dddc79
Evere77 <evere777@yahoo.com>
2023-04-08 15:11:41 -0700

    update

--- server.py
+++ server.py
@@ -6,17 +6,17 @@ app = Flask( __name__ )
 
 @app.route( "/play", methods=["GET"] )
 def display_play_ground():
-    return render_template( "index.html", many = 3) 
+    return render_template( "index.html", x = 3) 
 
 
 @app.route( "/play/<int:x>", methods = ["GET"] )
 def play_x(x):
-    return render_template("index.html", many = x )
+    return render_template("index.html", x = x )
 
 
-@app.route( "/play/<int:x>/<string:color>)", methods = ["GET"] )
+@app.route( "/play/<int:x>/<string:color>", methods = ["GET"] )
 def dif_color(x, color):
-    return render_template( "index.html", many = x, color = color )
+    return render_template( "index.html", x = x, color = color )
 
 
 
